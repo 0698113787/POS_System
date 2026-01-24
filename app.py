aa9e71f2665bfe9fdd7673fab304049a9fcd22c5
Andile Mgazi <vuyiswaandile176@gmail.com>
2026-01-25 01:18:45 +0200

    adding readme and app.run

--- app.py
+++ app.py
@@ -1011,4 +1011,5 @@ if __name__ == '__main__':
     print("Admin: admin / admin123")
     print("Puncher: puncher / stock123")
     print("=" * 50)
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port,debug=True)
